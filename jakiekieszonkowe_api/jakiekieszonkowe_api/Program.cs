7764163f25723eb36f7723807f1dcfed8cd47a4b
Rafał Dębski <rafadeb171@student.polsl.pl>
2019-01-02 20:51:14 +0100

    prototype of api

---
+++ jakiekieszonkowe_api/jakiekieszonkowe_api/Program.cs
@@ -0,0 +1,36 @@
+﻿using jakiekieszonkowe_api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace jakiekieszonkowe_api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+            CommentsController asd = new CommentsController();
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                var assd2 = asd.GetAllProducts();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
