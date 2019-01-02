f1c610803e8e969ef940128d4ea22d82a204495f
Rafał Dębski <rafadeb171@student.polsl.pl>
2019-01-02 21:18:47 +0100

    Fixed not building project

--- jakiekieszonkowe_api/jakiekieszonkowe_api/CommentsController.cs
+++ jakiekieszonkowe_api/jakiekieszonkowe_api/CommentsController.cs
@@ -6,11 +6,10 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Cors;
 
 namespace jakiekieszonkowe_api.Controllers
 {
-    [EnableCors(origins: "http://mywebclient.azurewebsites.net", headers: "*", methods: "*")]
+    
     public class CommentsController : ApiController
     {
         Comment_city[] comments_city = new Comment_city[]
